2a52afbdefe709131e2b2b3a2a9284d76c714f0e
ratna <ratnakumar.nakka@gmail.com>
2023-03-26 13:48:53 +0530

    rename module "api_gw"

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-module "eks_api_gw" {
+module "api_gw" {
     source = "github.com/ratnakumar-nakka/tf_module_Amazon_API_Gateway/"
     name = var.api_gw_name 
     create_api_domain_name = var.create_api_domain_name 
