436be66e9848099f3c7877fafd5fafa4c164721b
ratna <ratnakumar.nakka@gmail.com>
2023-03-26 14:27:30 +0530

    Added ec2

--- main.tf
+++ main.tf
@@ -1,9 +1,18 @@
 module "api_gw" {
-    source = "github.com/ratnakumar-nakka/tf_module_Amazon_API_Gateway/"
-    name = var.api_gw_name 
-    create_api_domain_name = var.create_api_domain_name 
+  source                 = "github.com/ratnakumar-nakka/tf_module_Amazon_API_Gateway/"
+  name                   = var.api_gw_name
+  create_api_domain_name = var.create_api_domain_name
 }
 
-module "eks" {
-    source = "github.com/ratnakumar-nakka/tf_module_Amazon_Elastic_Kubernetes_Service/"
+# module "eks" {
+#     source = "github.com/ratnakumar-nakka/tf_module_Amazon_Elastic_Kubernetes_Service/"
+# }
+
+resource "aws_instance" "web" {
+  ami           = "ami-0376ec8eacdf70aae"
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "HelloWorld"
+  }
 }
