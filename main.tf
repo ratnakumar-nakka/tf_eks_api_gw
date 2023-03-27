087fdb46ee7b9fc35729c98ac55a1e881ca34fbb
ratna <ratnakumar.nakka@gmail.com>
2023-03-27 22:20:48 +0530

    removed API gw

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
-module "api_gw" {
-  source                 = "github.com/ratnakumar-nakka/tf_module_Amazon_API_Gateway/"
-  name                   = var.api_gw_name
-  create_api_domain_name = var.create_api_domain_name
-}
+# module "api_gw" {
+#   source                 = "github.com/ratnakumar-nakka/tf_module_Amazon_API_Gateway/"
+#   name                   = var.api_gw_name
+#   create_api_domain_name = var.create_api_domain_name
+# }
 
 # # module "eks" {
 # #     source = "github.com/ratnakumar-nakka/tf_module_Amazon_Elastic_Kubernetes_Service/"
