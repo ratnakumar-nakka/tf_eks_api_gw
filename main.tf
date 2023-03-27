d4e46987c4b9c6ad72fee40a174e604802c7820d
ratna <ratnakumar.nakka@gmail.com>
2023-03-27 21:49:40 +0530

    Removed resources

--- main.tf
+++ main.tf
@@ -1,18 +1,18 @@
-module "api_gw" {
-  source                 = "github.com/ratnakumar-nakka/tf_module_Amazon_API_Gateway/"
-  name                   = var.api_gw_name
-  create_api_domain_name = var.create_api_domain_name
-}
-
-# module "eks" {
-#     source = "github.com/ratnakumar-nakka/tf_module_Amazon_Elastic_Kubernetes_Service/"
+# module "api_gw" {
+#   source                 = "github.com/ratnakumar-nakka/tf_module_Amazon_API_Gateway/"
+#   name                   = var.api_gw_name
+#   create_api_domain_name = var.create_api_domain_name
 # }
 
-resource "aws_instance" "web" {
-  ami           = "ami-0376ec8eacdf70aae"
-  instance_type = "t2.micro"
+# # module "eks" {
+# #     source = "github.com/ratnakumar-nakka/tf_module_Amazon_Elastic_Kubernetes_Service/"
+# # }
 
-  tags = {
-    Name = "HelloWorld"
-  }
-}
+# resource "aws_instance" "web" {
+#   ami           = "ami-0376ec8eacdf70aae"
+#   instance_type = "t2.micro"
+
+#   tags = {
+#     Name = "HelloWorld"
+#   }
+# }
